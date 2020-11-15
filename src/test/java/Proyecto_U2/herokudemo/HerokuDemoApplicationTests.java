3881cdd10fe28a378d3ebe13b3730e505e29cdfa
Nerinsky <74480258+Nerinsky@users.noreply.github.com>
2020-11-14 23:02:28 -0600

    Add files via upload

---
+++ src/test/java/Proyecto_U2/herokudemo/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto_U2.herokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
